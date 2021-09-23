8ca64abbce6540935f271a1fbaea741793ba7c36
Abilat01 <Abilat01@yandex.ru>
2021-09-23 17:17:29 +0300

    TableView

--- To-DoFirebase/LoginViewController.swift
+++ To-DoFirebase/LoginViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+    
 }
 
--- To-DoFirebase/TasksViewController.swift
+++ To-DoFirebase/TasksViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class TasksViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let tasks: [String] = ["Cell", "f", "fd", "fd"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func newTasksTapped(_ sender: UIBarButtonItem) {
     }
-    */
+    
+    
+}
 
+extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell #\(indexPath.row)"
+        
+        return cell
+    }
+    
 }
